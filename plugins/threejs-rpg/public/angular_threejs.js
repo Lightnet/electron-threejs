3c56eef95e353c17ec31106ee750cd490f5e0e2d
John <darkneter@gmail.com>
2016-04-08 14:14:42 -0700

    clean up and refresh props type build for cube edit position real time. keeping the script simple.

--- plugins/threejs-rpg/public/angular_threejs.js
+++ plugins/threejs-rpg/public/angular_threejs.js
@@ -1,109 +1,43 @@
 
-var threejsangular = angular.module('changeExample', []);
+var threejsangular = angular.module('threejsangular', []);
 
-threejsangular.controller('ExampleController', ['$scope', function($scope) {
-      $scope.counter = 0;
-	  console.log('test');
-      $scope.change = function() {
-		console.log($scope);
-        $scope.counter++;
-      };
-}]);
-
-threejsangular.controller('ControllerText', ['$scope', function($scope) {
-	console.log('text?');
-	$scope.init = function(params)
-  	{
-		$scope.params = params;
-		$scope.name = _.get(cube, params);
-		console.log($scope.name);
-  	}
-	//$scope.name = 'Whirled';
-	//console.log($scope);
-}]);
-
-threejsangular.controller('ControllerBoolean', ['$scope', function($scope) {
-	//console.log('test');
-	$scope.init = function(params)
-  	{
-		$scope.params = params;
-		$scope.confirmed = _.get(cube, params);
-  	}
-	$scope.change = function() {
-	  //console.log($scope.confirmed);
-	  //console.log(cube);
-	  _.set(cube, $scope.params, $scope.confirmed);
-	};
-}]);
-
-threejsangular.controller('ControllerInput', ['$scope', function($scope) {
-	//console.log($scope);
-	$scope.init = function(params)
-  	{
-		$scope.params = params;
-		$scope.value = _.get(cube, params);
-  	}
-	$scope.inputchange = function() {
-		_.set(cube, $scope.params, parseFloat($scope.value));
-		console.log(cube);
-		console.log($scope);
-	};
-}]);
-
-
+var injector = angular.injector();
 
 //http://jsfiddle.net/ftfish/KyEr3/
 
-
-/*
-function ControllerNodeObject($scope,$element,$attrs){
-	$scope.count++;
-	console.log('$scope?');
-	console.log($scope);
-	console.log($element);
-	console.log($attrs);
-	$element.bind("click", function(){
-		console.log('add');
-		scope.count++;
-		angular.element(document.getElementById('space-for-buttons')).append($compile("<div><button class='btn btn-default' data-alert="+scope.count+">Show alert #"+scope.count+"</button></div>")(scope));
-	});
-}
-*/
-
-threejsangular.component('nodecomponent', {
+//<div ng-controller="MainCtrl as vm">
+//<nodeinputcomponent params="vm.params"></nodeinputcomponent> //passing variable
+//<div>
+threejsangular.controller('NodeCtrl', function NodeCtrl($scope) {
+  var self= this;
+  $scope.init = function(params){
+	  self.params = params;
+	  console.log(self.params);
+  }
+});
+//===============================================
+// Node label
+//===============================================
+//<nodeinputcomponent params="params='name'"></nodeinputcomponent>
+threejsangular.component('nodelabelcomponent', {
 	bindings: {
-		hero: '=',
-		count: '='
+		params:'=' //bind from element attribute
 	},
-  	controller:'CounterCtrl',
+	scope: {
+		value:'=' //local var
+	},
+  	controller:'nodelabelCtrl',
   	template: function($element, $attrs){
-		//
-		return `<button addbuttons>Click to add buttons</button>`+
-		`<button listcompement ng-click="AppendText();">Refresh</button>` +
-		`<input type="text" ng-model="$ctrl.count">` +
-		`<button type="button" ng-click="$ctrl.decrement();">-</button>` +
-		`<button type="button" ng-click="$ctrl.increment();">+</button>`
-		;
+		return `<div>`+
+					`<label>{{$ctrl.params}}</label>:`+
+					`<label>{{$ctrl.value}}<label>` +
+				`</div>`;
 	}
-}).controller('CounterCtrl', function CounterCtrl($scope) {
-	$scope.AppendText = function() {
-		var myEl = angular.element( document.querySelector( '#listcomponent' ) );
-		//myEl.append('Hi<br/>');
-		//myEl.append('<data-my-template/>');
-
-		if(cube['uuid'] != null){
-			console.log(cube['uuid']);
-			myEl.append($scope(`<div ng-controller="ControllerText" ng-init="init('uuid')"><span ng-bind="name"></span></div>`));
-		}
-		if(cube['name'] != null){
-			myEl.append($scope(`<div ng-controller="ControllerInput">`+
-				`<input type="input" ng-change="inputchange()" ng-model="value" ng-value="value" ng-init="init('name')" />`+
-				`</div>`));
-		}
-	}
+}).controller('nodelabelCtrl', function nodelabelCtrl($scope) {
 	this.$onInit = function () {
-    	// component initialisation
-		console.log($scope);
+		// component initialisation
+		this.value = _.get(cube, this.params);
+		console.log(this);
   	};
   	this.$postLink = function () {
     	// component post-link
@@ -111,153 +45,196 @@ threejsangular.component('nodecomponent', {
   	this.$onDestroy = function () {
     	// component $destroy function
   	};
-  	function increment() {
-    	this.count++;
-  	}
-  	function decrement() {
-    	this.count--;
-  	}
-  	function refresh(){
-	  	console.log(this);
-  	}
-  	this.increment = increment;
-  	this.decrement = decrement;
-  	this.refresh = refresh;
-}).controller('MainCtrl', function MainCtrl() {
-  this.count = 4;
+});
+//===============================================
+// Node Input
+//===============================================
+//<nodeinputcomponent params="params='name'"></nodeinputcomponent>
+threejsangular.component('nodeinputcomponent', {
+	bindings: {
+		params:'=',
+		value:'='
+	},
+  	controller:'nodeinputCtrl',
+  	template: function($element, $attrs){
+		return `<div>`+
+					`<label>{{$ctrl.params}}</label>`+
+					`<input type="input" ng-model="value" ng-value="value" ng-change="$ctrl.change()" />` +
+				`</div>`;
+	}
+}).controller('nodeinputCtrl', function nodebooleanCtrl($scope) {
+	function change() {
+	  console.log($scope.$ctrl.params);
+	  _.set(cube, $scope.$ctrl.params, $scope.value);
+	  console.log(cube);
+	};
+	this.change = change;
+	this.$onInit = function () {
+		// component initialisation
+		console.log(this);
+		$scope.value = _.get(cube, $scope.$ctrl.params);
+  	};
+  	this.$postLink = function () {
+    	// component post-link
+  	};
+  	this.$onDestroy = function () {
+    	// component $destroy function
+  	};
 });
 
+//`<nodebooleancomponent params="params='visible'"></nodebooleancomponent>`
+threejsangular.component('nodebooleancomponent', {
+	bindings: {
+		params:'=',
+		confirmed:'='
+	},
+  	controller:'nodebooleanCtrl',
+  	template: function($element, $attrs){
+		return `<div>`+
+					`<label>{{$ctrl.params}}</label>`+
+					`<input type="checkbox" ng-model="confirmed" ng-change="$ctrl.change()" />` +
+				`</div>`;
+	}
+}).controller('nodebooleanCtrl', function nodebooleanCtrl($scope) {
+	function change() {
+	  //console.log($scope.$ctrl.params);
+	  _.set(cube, $scope.$ctrl.params, $scope.confirmed);
+	};
+	this.change = change;
+	this.$onInit = function () {
+		// component initialisation
+		$scope.confirmed = _.get(cube, $scope.$ctrl.params);
+  	};
+  	this.$postLink = function () {
+    	// component post-link
+  	};
+  	this.$onDestroy = function () {
+    	// component $destroy function
+  	};
+});
+
+//`<nodecomponent ></nodecomponent>`
+threejsangular.component('nodecomponent', {
+	bindings: {
+		//count: '='
+	},
+  	controller:'nodecomponentCtrl',
+  	template: function($element, $attrs){
+		//
+		return 	``+
+				`<button ng-click="$ctrl.refresh();">Ctrl Refresh</button>`+
+				`<button objectnodecomponets>Refresh</button>`+
+			   	`<button clearobjectnodes>Clear Props</button>`+
+				`<button clearobjectnodes onclick="orefresh();">Refresh Call</button>`+
+				`<button clearobjectnodes onclick="getscope();">Scope Refresh Call</button>`
+				;
+	}
+}).controller('nodecomponentCtrl', function nodecomponentCtrl($scope) {
+	function refresh() {
+		orefresh();
+		/*
+		console.log('refesh props');
+		var $target = document.getElementById('listcomponent');
+		var myEl = angular.element(
+			$target
+		);
+		myEl.empty();
+		//console.log(this);
+		angular.element($target).injector().invoke(function($compile) {
+    		var scope = angular.element($target).scope();
+    		angular.element($target).append($compile(`<nodelabelcomponent params="params='uuid'"></nodelabelcomponent>`)(scope));
+    		// Finally, refresh the watch expressions in the new element
+    		//scope.$apply();
+			console.log('add?');
+  		});
+		*/
+	}
+	this.refresh = refresh;
+	this.$onInit = function () {
+    	// component initialisation
+		//console.log($scope);
+  	};
+  	this.$postLink = function () {
+    	// component post-link
+  	};
+  	this.$onDestroy = function () {
+    	// component $destroy function
+  	};
+});
+
+threejsangular.directive("clearobjectnodes", function($compile){
+	return function(scope, element, attrs){
+		element.bind("click", function(){
+			//scope.count++;
+			//console.log('add?');
+			var myEl = angular.element(
+				document.getElementById('listcomponent')
+			);
+			myEl.empty();
+		});
+	};
+});
 
 //Directive for adding buttons on click that show an alert on click
-threejsangular.directive("addbuttons", function($compile){
+threejsangular.directive("objectnodecomponets", function($compile){
 	return function(scope, element, attrs){
 		element.bind("click", function(){
 			//scope.count++;
 			console.log('add?');
-			//var myEl = angular.element( document.querySelector( '#listcomponent' ) );
-			//myEl.append($compile(`<div ng-controller="ControllerText" ng-init="init('uuid')"><span ng-bind="name"></span></div>`)(scope));
-
-			//angular.element(
-				//document.getElementById('listcomponent')
-			//).append($compile("<div><button class='btn btn-default' data-alert="+scope.count+">Show alert #"+scope.count+"</button></div>")(scope));
-
 			var myEl = angular.element(
 				document.getElementById('listcomponent')
 			);
-			//myEl.append($compile(`<div ng-controller="ControllerText" ng-init="init('uuid')"><span ng-bind="name"></span></div>`)(scope));
-			myEl.append($compile(`<div><div ng-controller="ControllerText" ng-init="init('uuid')"><span ng-bind="name"></span></div></div>`)(scope));
+			//clear children
+			myEl.empty();
 
-			myEl.append($compile(`<div ng-controller="ControllerBoolean">`+
-				`<input type="checkbox" ng-model="confirmed" ng-change="change()" ng-init="init('visible')" />`+
-			`</div>`)(scope));
-
+			if(cube['uuid'] !=null){
+				myEl.append($compile(`<nodelabelcomponent params="params='uuid'"></nodelabelcomponent>`)(scope));
+			}
+			if(cube['visible'] !=null){
+				myEl.append($compile(`<nodebooleancomponent params="params='visible'"></nodebooleancomponent>`)(scope));
+			}
+			if(cube['position'] !=null){
+				myEl.append($compile(`<nodeinputcomponent params="params='position.x'"></nodeinputcomponent>`)(scope));
+				myEl.append($compile(`<nodeinputcomponent params="params='position.y'"></nodeinputcomponent>`)(scope));
+				myEl.append($compile(`<nodeinputcomponent params="params='position.z'"></nodeinputcomponent>`)(scope));
+			}
 		});
 	};
 });
 
-
-
-/*
-threejsangular.directive('listnodecompement', function($compile){
-	return function(scope, element, attrs){
-		element.bind("click", function(){
-			console.log('add');
-			scope.count++;
-			angular.element(document.getElementById('space-for-buttons')).append($compile("<div><button class='btn btn-default' data-alert="+scope.count+">Show alert #"+scope.count+"</button></div>")(scope));
+function orefresh(){
+	setTimeout(function () {
+		var $target = document.getElementById('listcomponent');
+		var myEl = angular.element(
+			$target
+		);
+		myEl.empty();
+		//console.log(this);
+		angular.element($target).injector().invoke(function($compile) {
+			var scope = angular.element($target).scope();
+			angular.element($target).append($compile(`<nodelabelcomponent params="params='uuid'"></nodelabelcomponent>`)(scope));
+			// Finally, refresh the watch expressions in the new element
+			scope.$apply();
+			console.log('add?');
 		});
-	};
-});
-*/
+	}, 50);
+}
 
-/*
-function addController($compile){
-   return function(scope, element, attrs){
-	   element.bind("click", function(){
-		   scope.count++;
-		   angular.element(document.getElementById('space-for-buttons')).append($compile("<div><button class='btn btn-default' data-alert="+scope.count+">Show alert #"+scope.count+"</button></div>")(scope));
-	   });
-};
-
-threejsangular.component('addbuttons', {
-  //templateUrl: 'heroDetail.html',
-  //controller: addController,
-  //bindings: {
-    //hero: '='
-  //}
-  template: "<button addbuttons>Click to add buttons</button>"
-});
-*/
-
-
-
-
-threejsangular.controller('ControllerSceneObject', ['$scope', function($scope) {
-	//$scope.items = [{'item':'test0'},{'item':'test1'},{'item':'test2'}];
-	$scope.Refresh = function() {
-		console.log('ControllerSceneObject');
-		if(cube['name'] !=null){
-
-		}
-	}
-	$scope.additem = function() {
-	  console.log($scope);
-	  //$scope.items.push({'item':'test0'});
-	};
-}]);
 //http://jsfiddle.net/austinnoronha/nukRe/light/
-
 function getscope(){
-	var scope = angular.element(document.getElementById('yourControllerElementID')).scope();
-	console.log(scope);
+	setTimeout(function () {
+		var $target = document.getElementById('listcomponent');
+		var myEl = angular.element(
+			$target
+		);
+		myEl.empty();
+		//console.log(this);
+		angular.element($target).injector().invoke(function($compile) {
+			var scope = angular.element($target).scope();
+			angular.element($target).append($compile(`<nodelabelcomponent params="params='uuid'"></nodelabelcomponent>`)(scope));
+			// Finally, refresh the watch expressions in the new element
+			scope.$apply();
+			console.log('add?');
+		});
+	}, 50);
 }
-
-threejsangular.controller('ExampleControllerAdd', ['$scope', function($scope) {
-	$scope.items = [{'item':'test0'},{'item':'test1'},{'item':'test2'}];
-	$scope.additem = function() {
-	  console.log($scope);
-	  $scope.items.push({'item':'test0'});
-	};
-}]);
-
-
-
-console.log(angular);
-
-function HeroDetailController() {
-
-}
-
-threejsangular.component('heroDetail', {
-  templateUrl: 'heroDetail.html',
-  controller: HeroDetailController,
-  bindings: {
-    hero: '='
-  }
-});
-
-
-/*
-var Counter = ang
-.component({
-  selector: 'counter',
-  template: [
-    '<div class="todo">',
-      '<input type="text" [(ng-model)]="count">',
-      '<button type="button" (click)="decrement();">-</button>',
-      '<button type="button" (click)="increment();">+</button>',
-    '</div>'
-  ].join('')
-})
-.Class({
-  constructor: function () {
-    this.count = 0;
-  },
-  increment: function () {
-    this.count++;
-  },
-  decrement: function () {
-    this.count--;
-  }
-});
-*/
-//
